3bd8d13e8988fe59ba4cdee8b6c2d085ab54d22f
martinh <mhofmann@senseinode.com>
2024-07-04 12:23:12 +0300

    feature: created wallet connection to fetch balances

---
+++ blockchain/wallet.py
@@ -0,0 +1,10 @@
+from web3 import Web3
+from config.settings import WEB3_PROVIDER_URL
+
+# Connect to a blockchain node (Infura, local node, etc.)
+web3 = Web3(Web3.HTTPProvider(WEB3_PROVIDER_URL))
+
+def get_balance(address):
+    checksum_address = web3.to_checksum_address(address)
+    balance = web3.eth.get_balance(checksum_address)
+    return web3.from_wei(balance, 'ether')
